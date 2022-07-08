6cf3bef732732f9ef5e65721253cfdb99650c797
LoureiroBruno <brunoloureiro9@yahoo.com.br>
2022-07-07 22:45:20 -0300

    compose update

--- buscar_cursos.php
+++ buscar_cursos.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace GuzzleHttp;
+// Chamada ao autoload do composer
+require_once __DIR__ . '/vendor/autoload.php';
 
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
-$cliente = new Client();
-$retorno = $cliente->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+$cliente = new Client;
+$retorno = $cliente->request('GET', 'https://cursos.alura.com.br/category/programacao/php');
 
-echo "teste";
+$html = $retorno->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+
 ?>
